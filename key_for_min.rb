a07b9fe8de28d71319a14a4eb42f4851f4f95eb2
Francis Mendez <fmendez884@gmail.com>
2019-04-17 15:08:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,9 @@ def key_for_min_value(name_hash)
   if name_hash.empty? 
     return nil
   elsif 
-  name_hash.each do |key, value|
-     value.each do |a, b|
-       if a > b 
-         return a 
-       end 
-     end
-  end 
-  end 
-end
+    name_hash.each do |key, value|
+      return key 
+    end
+  end
+end 
+  
